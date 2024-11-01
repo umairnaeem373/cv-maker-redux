6dee7dd1738e74615a1971664d211ef22823e627
umairnaeem373 <umairnaeem373@gmail.com>
2024-11-02 02:04:54 +0500

    tailwind error fixed

--- src/Components/HomePage/UserProfiles.jsx
+++ src/Components/HomePage/UserProfiles.jsx
@@ -7,7 +7,7 @@ function UserProfiles() {
         User profiles
       </h1>
       <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-4 mt-[60px] container mx-auto">
-        <div className="list-none *:border *:border-green-600 flex flex-wrap h-fit w-fit">
+        <div className="list-none flex flex-wrap h-fit w-fit">
           <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
             <span className="w-[66px] whitespace-nowrap text-[#373737]">

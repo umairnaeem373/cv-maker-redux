a3a80f88817bae6dfd91d8776f02ae8850734617
umairnaeem373 <umairnaeem373@gmail.com>
2024-11-02 01:15:29 +0500

    User Profiles

--- src/Components/HomePage/UserProfiles.jsx
+++ src/Components/HomePage/UserProfiles.jsx
@@ -6,96 +6,136 @@ function UserProfiles() {
       <h1 className="text-[#373737] text-4xl text-center mt-[155px] font-bold leading-[24px]">
         User profiles
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 border  mt-[60px] mx-[237px]">
-        <div className="list-none flex flex-wrap *: border h-fit w-fit">
-          <li className="w-full border flex justify-center items-center">
+      <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-4 mt-[60px] container mx-auto">
+        <div className="list-none *:border *:border-green-600 flex flex-wrap h-fit w-fit">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap  text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
-          </li>
-        </div>
-
-        <div className="list-none flex flex-wrap content-between h-[182px] w-[103px]">
-          <li className="w-full flex justify-between items-center">
-            <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
-          </li>
-          <li className="w-full flex justify-between items-center">
-            <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
-          </li>
-          <li className="w-full flex justify-between items-center">
-            <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
-          </li>
-          <li className="w-full flex justify-between items-center">
-            <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
-          </li>
-          <li className="w-full flex justify-between items-center">
-            <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
         </div>
 
-        <div className="list-none flex flex-wrap content-between h-[182px] w-[103px]">
-          <li className="w-full flex justify-between items-center">
+        <div className="list-none flex flex-wrap h-fit w-fit">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
         </div>
 
-        <div className="list-none flex flex-wrap content-between h-[182px] w-[103px]">
-          <li className="w-full flex justify-between items-center">
+        <div className="list-none flex flex-wrap h-fit w-fit">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
-          <li className="w-full flex justify-between items-center">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
             <img src="../Images/Avatar.png" alt="" />
-            <span className="w-[66px] whitespace-nowrap text-[#373737]">John Doe</span>
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
+          </li>
+        </div>
+
+        <div className="list-none flex flex-wrap h-fit w-fit">
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
+            <img src="../Images/Avatar.png" alt="" />
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
+          </li>
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
+            <img src="../Images/Avatar.png" alt="" />
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
+          </li>
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
+            <img src="../Images/Avatar.png" alt="" />
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
+          </li>
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
+            <img src="../Images/Avatar.png" alt="" />
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
+          </li>
+          <li className="w-full p-4 flex gap-4 justify-center items-center">
+            <img src="../Images/Avatar.png" alt="" />
+            <span className="w-[66px] whitespace-nowrap text-[#373737]">
+              John Doe
+            </span>
           </li>
         </div>
       </div>
